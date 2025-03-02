1bbed12be333fdb8a8ea6809d73d27ee6f865d46
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-03-03 01:18:58 +0700

    Initialize multiprocessing pool for parallel processing

--- imgproc.py
+++ imgproc.py
@@ -21,3 +21,5 @@ def main(input_folder, output_folder):
         os.makedirs(output_folder)
 
         image_files = [os.path.join(input_folder, f) for f in os.listdir(input_folder) if f.endswith(('jpg', 'png', 'jpeg'))]
+        
+        with multiprocessing.Pool() as pool:
