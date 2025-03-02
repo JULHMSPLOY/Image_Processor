54d2ba951af49cc0ef228679a79f1c489f013763
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-03-03 01:05:10 +0700

    Extract filename from image path

--- imgproc.py
+++ imgproc.py
@@ -7,3 +7,5 @@ def process_image(image_path, output_folder):
         with Image.open(image_path) as img:
             img_gray = img.convert('L')
             img_resized = img_gray.resize((128, 128))
+
+            filename = os.path.basename(image_path)
