457be615559d27484bc64260d6bf3741d7e9faf7
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-03-03 01:19:42 +0700

    Process images in parallel using multiprocessing pool

--- imgproc.py
+++ imgproc.py
@@ -23,3 +23,4 @@ def main(input_folder, output_folder):
         image_files = [os.path.join(input_folder, f) for f in os.listdir(input_folder) if f.endswith(('jpg', 'png', 'jpeg'))]
         
         with multiprocessing.Pool() as pool:
+            pool.starmap(process_image, [(image, output_folder) for image in image_files])
