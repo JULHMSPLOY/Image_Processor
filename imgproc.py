bdac832233be128eb6bc5e1c7c52e463eade7001
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-03-03 00:56:05 +0700

    Resize image to 128x128 pixels

--- imgproc.py
+++ imgproc.py
@@ -6,3 +6,4 @@ def process_image(image_path, output_folder):
     try:
         with Image.open(image_path) as img:
             img_gray = img.convert('L')
+            img_resized = img_gray.resize((128, 128))
